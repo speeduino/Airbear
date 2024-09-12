7ee21d5cf4e390e0229e03925c083a7b5e501c29
Josh Stewart <josh@noisymime.org>
2024-09-12 14:56:37 +1000

    Add fullscreen button to dash

--- data/js/gauges.js
+++ data/js/gauges.js
@@ -212,6 +212,34 @@ var gaugeConfig_qtrLeft = {
     animationRule: "linear",
     valueBox: true,
 };
+
+function toggle_fullscreen() {
+    if (!document.fullscreenElement) {
+        document.body.requestFullscreen();
+        document.body.setAttribute("fullscreen",""); 
+    } else {
+        document.exitFullscreen();
+        document.body.removeAttribute("fullscreen"); 
+    }
+} 
+
+if (document.fullscreenEnabled) {
+    const fullscreen_button = document.createElement("button");
+    fullscreen_button.setAttribute('id','fullscreen-button');
+    fullscreen_button.addEventListener("click", toggle_fullscreen);
+    fullscreen_button.innerHTML  = `
+        <svg viewBox="0 0 24 24">
+            <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 
+            7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"/>
+        </svg>
+        <svg viewBox="0 0 24 24">
+            <path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 
+            11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"/>
+        </svg>
+    `;
+    document.body.appendChild(fullscreen_button);
+}
+
 const gauges = [];
 
 gauges.push(new RadialGauge(Object.assign( {renderTo: 'gauge1' }, gaugeConfig_rpm2)));
